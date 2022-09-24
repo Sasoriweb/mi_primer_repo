1e24c3f45a5e5e12d2dcc1565a2cbc836c8276b4
Paulo Sandoval <paulo@topsort.com>
2022-09-24 13:07:27 -0300

    Cambie la estructura a una lista y ademas la salida del nivel 1 tiene un formato humano

--- escrito.py
+++ escrito.py
@@ -1,22 +1,25 @@
 escritura1 = {"palabra" : "かさ", "significado" : "paraguas", "silabario" : "hiragana", "origen" : "Japón"}
 escritura2 = {"palabra" : "ケーキ", "significado" : "torta", "silabario" : "katakana", "origen" : "Japón"}
 escritura3 = {"palabra" : "本", "significado" : "libro","silabario" : "kanji", "origen" : "China"}
+escritura4 = {"palabra" : "日", "significado" : "día" ,"silabario" : "kanji", "origen" : "China"}
 
+lista_escrituras = [escritura1, escritura2, escritura3, escritura4]
 
 def organizar_escritura(escritura):
     es_nivel_1 = escritura["silabario"] == "hiragana" 
     es_nivel_2 = escritura["silabario"] == "katakana"
     es_nivel_3 = escritura["silabario"] == "kanji"
     palabra = escritura["palabra"]
+    significado = escritura["significado"]
+    origen = escritura["origen"]
+    silabario = escritura["silabario"]
     if es_nivel_1 and not es_nivel_2:
-        return f"{escritura1} Nivel básico A"
+        return f"La palabra {palabra} significa {significado}, pertenece al silabario {silabario} y es de origen {origen}"
     elif es_nivel_2:
-        return f"{escritura2} Nivel básico B"
+        return f"{escritura} Nivel básico B"
     else:
-        return f"{escritura3} Nivel Avanzado"
+        return f"{escritura} Nivel Avanzado"
 
-print(organizar_escritura(escritura1))
-print(organizar_escritura(escritura2))
-print(organizar_escritura(escritura3))
-
+for escritura in lista_escrituras:
+    print(organizar_escritura(escritura))
 
